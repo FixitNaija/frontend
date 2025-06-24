27e1afc6303a61bcdbd1280cb24accfb49eb3ac9
teecrown <teecrown@gmail.com>
2025-06-24 20:55:27 +0000

    new

--- src/Pages/UserPage.jsx
+++ src/Pages/UserPage.jsx
@@ -24,7 +24,7 @@ const Dashboard = () => {
     useEffect(() => {
         const storedUsername = localStorage.getItem('fixitnaija_username');
         if (storedUsername) {
-            setUsername(storedUsername);
+            // setUsername(storedUsername);
         }
     }, []);
 
